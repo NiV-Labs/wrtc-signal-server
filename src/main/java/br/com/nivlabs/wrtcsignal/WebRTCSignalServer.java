8543f8f9e941c9ace2e64b401164e214acc0e166
viniciosarodrigues <viniciosarodrigues@gmail.com>
2022-08-05 22:22:25 -0300

    Criação do projeto

---
+++ src/main/java/br/com/nivlabs/wrtcsignal/WebRTCSignalServer.java
@@ -0,0 +1,13 @@
+package br.com.nivlabs.wrtcsignal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRTCSignalServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRTCSignalServer.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nivlabs/wrtcsignal/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nivlabs.wrtcsignal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
